18a2fe25b43a0e85d6753d214e2d09c5311caf4a
gamalasker <gamalasker@gmail.com>
2020-04-18 19:47:31 +0200

    1

---
+++ quiz.js
@@ -0,0 +1,526 @@
+﻿ var quiztitle = "Exercices";
+
+
+ var quiz = [
+        {
+            "question" : "Mon père a …travail, il est fatigué. ",
+            "image" : "",
+            "choices" : [
+                                    "Peu de",
+                                    "Beaucoup de",
+                                    "Un peu",
+                                ],
+            "correct" : "Beaucoup de",
+            "explanation" : "la bonne réponse est : Beaucoup de",
+        },
+        {
+            "question" : "Pour la recette de la crêpe,  il faut ….farine que de sucre.",
+            "image" : "",
+            "choices" : [
+                                    "Plus de",
+                                    "Moins de",
+                                    "Autant de",
+                                ],
+            "correct" : "Autant de",
+            "explanation" : "la bonne réponse est : Autant de",
+        },
+        {
+            "question" : "Il ne mange pas…bonbons que moi.",
+            "image" : "",
+            "choices" : [
+                                    "Plus de",
+                                    "Autant de",
+                                    "Moins de",
+                                ],
+            "correct" : "Autant de",
+            "explanation" : "la bonne réponse est : Autant de",
+        },
+        {
+            "question" : "On achète 200 grammes de viande ?Non, on en achète…! Il y a 8 personnes au déjeuner !",
+           "image" : "",
+            "choices" : [
+                                    "Plus",
+                                    "moins",
+                                    "Moins de",
+                                ],
+            "correct" : "Plus",
+            "explanation" : "la bonne réponse est : Plus ",
+        },
+        {
+            "question" : "Tu mets 5 pommes dans la recette ? Non, il faut 2 pommes, j'en mets…",
+            "image" : "",
+            "choices" : [
+                                    "Moins",
+                                    "Plus",
+                                    "Autant de"
+                                    
+                                ],
+            "correct" : "Moins",
+            "explanation" : "la bonne réponse est :Moins",
+        },
+        {
+            "question" : "Les gens riches ont…argent.",
+            "image" : "",
+            "choices" : [
+                                    "Beaucoup d’",
+                                    "Beaucoup de",
+                                    "Peu d’",
+                                ],
+            "correct" : "Beaucoup d’",
+            "explanation" : "la bonne réponse est : Beaucoup d’",
+        },
+        {
+            "question" : "Pour être en bonne santé, tu dois manger un…tout.",
+            "image" : "",
+            "choices" : [
+                                    "Peu de",
+                                    "Beaucoup de",
+                                    "peu",
+                                    
+                                ],
+            "correct" : "Peu de",
+            "explanation" : "la bonne réponse est : Peu de",
+        },
+        {
+            "question" : "Pendant les examens, il y a …étudiants à la bibliothèque. ",
+            "image" : "",
+            "choices" : [
+                                    "Peu de",
+                                    "Beaucoup ",
+                                    "Beaucoup de",
+                                   
+                                ],
+            "correct" : "Beaucoup d'",
+            "explanation" : "la bonne réponse est : Beaucoup d'",
+        },
+        {
+            "question" : "Faire un…sport chaque jour, c'est bon pour la santé.",
+            "image" : "",
+            "choices" : [
+                                    "Peu de",
+                                    "Beaucoup ",
+                                    "Peu",
+                                   
+                                ],
+            "correct" : "Peu de",
+            "explanation" : "la bonne réponse est : Peu de",
+        },
+        {
+            "question" : "….de croissants elle veut acheter ?",
+            "image" : "",
+            "choices" : [
+                                    "Peu",
+                                    "Combien ",
+                                    "Beaucoup ",
+                                    
+                                ],
+            "correct" : "Combien ",
+            "explanation" : "la bonne réponse est : Combien ",
+        },
+        {
+            "question" : "Elle étudie ...... que son amie.",
+            "image" : "",
+            "choices" : [
+                                    "plus de",
+                                    "plus que",
+                                    "moins",
+                                 
+                                ],
+            "correct" : "plus que",
+            "explanation" : "la bonne réponse est : plus que",
+        },
+        {
+            "question" : "Il lit….car il n'a pas de lunettes.",
+            "image" : "",
+            "choices" : [
+                                    "Un peu",
+                                    "peu de",
+                                    "beaucoup",
+                                   
+                                ],
+            "correct" : "Un peu",
+            "explanation" : "la bonne réponse est : Un peu ",
+        },
+        {
+            "question" : "Il y a beaucoup de posters dans ta chambre ? Oui, il y …a quatre. ",
+            "image" : "",
+            "choices" : [
+                                    "En",
+                                    "L'",
+                                    "Les",
+                                    
+                                ],
+            "correct" : "En",
+            "explanation" : "la bonne réponse est : En",
+        },
+        {
+            "question" : "Tu as beaucoup d'ananas dans ta salade de fruits ? Non, j'en mets …",
+            "image" : "",
+            "choices" : [
+                                    "Peu de",
+                                    "Peu",
+                                    "Un peu",
+                                   
+                                ],
+            "correct" : "Un peu",
+            "explanation" : "la bonne réponse est : Un peu",
+        },
+        {
+            "question" : "Le café, mon père en boit …le soir.",
+            "image" : "",
+            "choices" : [
+                                    "Beaucoup de ",
+                                    "Un peu",
+                                    "Peu",
+                                   
+                                ],
+            "correct" : "Peu",
+            "explanation" : "la bonne réponse est : Peu",
+        },
+        {
+            "question" : "J'ai….livres.",
+            "image" : "",
+            "choices" : [
+                                    "Quatre-vingt-dix",
+                                    "Quatre-vingts-dix ",
+                                    "Quatre-vingt",
+                                  
+                                ],
+            "correct" : "Quatre-vingt-dix",
+            "explanation" : "la bonne réponse est : Quatre-vingt-dix",
+        },
+        {
+            "question" : "Ils sont gourmands, ils mangent …",
+            "image" : "",
+            "choices" : [
+                                    "Plus",
+                                    "Beaucoup",
+                                    "Peu",
+                                   
+                                ],
+            "correct" : "Beaucoup",
+            "explanation" : "la bonne réponse est : Beaucoup",
+        },
+        {
+            "question" : "En France, on mange…fromage que de poisson. ",
+            "image" : "",
+            "choices" : [
+                                    "Plus que",
+                                    "Plus",
+                                    "Plus de",
+                                    
+                                ],
+            "correct" : "Plus de",
+            "explanation" : "la bonne réponse est :Plus de ",
+        },
+        {
+            "question" : "J'achète un litre…lait.",
+            "image" : "",
+            "choices" : [
+                                    "De",
+                                    "du",
+                                    "de la",
+                                 
+                                ],
+            "correct" : "De",
+            "explanation" : "la bonne réponse est : De",
+        },
+        {
+            "question" : "Elle achète un…beurre.",
+            "image" : "",
+            "choices" : [
+                                    "Kilo",
+                                    "litre",
+                                    "Morceau",
+                                   
+                                ],
+            "correct" : "Kilo",
+            "explanation" : "la bonne réponse est : Kilo",
+        },
+{
+            "question" : "Ça fait…?",
+            "image" : "",
+            "choices" : [
+                                    "Quand",
+                                    "Combien",
+                                    "Comment",
+                                   
+                                ],
+            "correct" : "Combien",
+            "explanation" : "la bonne réponse est : Combien",
+        },
+{
+            "question" : "1",
+            "image" : "assets/1.jpg",
+            "choices" : [
+                                    "moins",
+                                    "Plus",
+                                    "Autant de",
+                                   
+                                ],
+            "correct" : "Autant de",
+            "explanation" : "la bonne réponse est : Autant de",
+        },
+{
+            "question" : "2",
+            "image" : "assets/2.jpg",
+            "choices" : [
+                                    "autant",
+                                    "moins",
+                                    "Autant de",
+                                   
+                                ],
+            "correct" : "autant",
+            "explanation" : "la bonne réponse est : autant",
+        },
+{
+            "question" : "3",
+            "image" : "assets/3.jpg",
+            "choices" : [
+                                    "moins",
+                                    "plus",
+                                    "Autant de",
+                                   
+                                ],
+            "correct" : "plus",
+            "explanation" : "la bonne réponse est : plus",
+        },
+{
+            "question" : "4",
+            "image" : "assets/4.jpg",
+            "choices" : [
+                                    "moins",
+                                    "autant",
+                                    "plus",
+                                   
+                                ],
+            "correct" : "plus",
+            "explanation" : "la bonne réponse est : plus",
+        },
+{
+            "question" : "Cinq mille cent trente-sept =...",
+            "image" : "",
+            "choices" : [
+                                    "5137",
+                                    "5138",
+                                    "517",
+                                   
+                                ],
+            "correct" : "5137",
+            "explanation" : "la bonne réponse est : 5137",
+        },
+{
+            "question" : "Neuf mille trois cent quatre-vingt-dix",
+            "image" : "",
+            "choices" : [
+                                    "939",
+                                    "639",
+                                    "9390",
+                                   
+                                ],
+            "correct" : "9390",
+            "explanation" : "la bonne réponse est : 9390",
+        },
+{
+            "question" : "Trois mille deux cent cinquante-cinq",
+            "image" : "",
+            "choices" : [
+                                    "3255",
+                                    "3254",
+                                    "3250",
+                                   
+                                ],
+            "correct" : "3255",
+            "explanation" : "la bonne réponse est : 3255",
+        },
+{
+            "question" : "Deux mille quarante-trois",
+            "image" : "",
+            "choices" : [
+                                    "243",
+                                    "2043",
+                                    "2044",
+                                   
+                                ],
+            "correct" : "2043",
+            "explanation" : "la bonne réponse est : 2043",
+        },
+{
+            "question" : "Mille sept cent soixante-douze",
+            "image" : "",
+            "choices" : [
+                                    "1772",
+                                    "177",
+                                    "17722",
+                                   
+                                ],
+            "correct" : "1772",
+            "explanation" : "la bonne réponse est : 1772",
+        },
+        
+    ];
+
+
+ var currentquestion = 0,
+     score = 0,
+     submt = true,
+     picked;
+
+ jQuery(document).ready(function ($) {
+
+
+     function htmlEncode(value) {
+         return $(document.createElement('div')).text(value).html();
+     }
+
+
+     function addChoices(choices) {
+         if (typeof choices !== "undefined" && $.type(choices) == "array") {
+             $('#choice-block').empty();
+             for (var i = 0; i < choices.length; i++) {
+                 $(document.createElement('li')).addClass('choice choice-box').attr('data-index', i).text(choices[i]).appendTo('#choice-block');
+             }
+         }
+     }
+
+     function nextQuestion() {
+         submt = true;
+         $('#explanation').empty();
+         $('#question').text(quiz[currentquestion]['question']);
+         $('#pager').text('Question ' + Number(currentquestion + 1) + ' of ' + quiz.length);
+         if (quiz[currentquestion].hasOwnProperty('image') && quiz[currentquestion]['image'] != "") {
+             if ($('#question-image').length == 0) {
+                 $(document.createElement('img')).addClass('question-image').attr('id', 'question-image').attr('src', quiz[currentquestion]['image']).attr('alt', htmlEncode(quiz[currentquestion]['question'])).insertAfter('#question');
+             } else {
+                 $('#question-image').attr('src', quiz[currentquestion]['image']).attr('alt', htmlEncode(quiz[currentquestion]['question']));
+             }
+         } else {
+             $('#question-image').remove();
+         }
+         addChoices(quiz[currentquestion]['choices']);
+         setupButtons();
+
+
+     }
+
+
+     function processQuestion(choice) {
+         if (quiz[currentquestion]['choices'][choice] == quiz[currentquestion]['correct']) {
+             $('.choice').eq(choice).css({
+                 'background-color': '#50D943'
+             });
+             $('#explanation').html('<strong>Correct!</strong> ' + htmlEncode(quiz[currentquestion]['explanation']));
+             score++;
+         } else {
+             $('.choice').eq(choice).css({
+                 'background-color': '#D92623'
+             });
+             $('#explanation').html('<strong>Incorrect!</strong> ' + htmlEncode(quiz[currentquestion]['explanation']));
+         }
+         currentquestion++;
+         $('#submitbutton').html('question suivante &raquo;').on('click', function () {
+             if (currentquestion == quiz.length) {
+                  endQuiz();
+             } else {
+                 $(this).text('Valider').css({
+                     'color': '#fff'
+                 }).off('click');
+                 nextQuestion();
+             }
+         });
+         if (currentquestion == quiz.length) {
+             $('#submitbutton').html('Finir').css({
+                 'color': '#fff'
+             });  
+         }
+     }
+
+
+     function setupButtons() {
+         $('.choice').on('mouseover', function () {
+             $(this).css({
+                 'background-color': '#e1e1e1'
+             });
+         });
+         $('.choice').on('mouseout', function () {
+             $(this).css({
+                 'background-color': '#fff'
+             });
+         })
+         $('.choice').on('click', function () {
+             picked = $(this).attr('data-index');
+             $('.choice').removeAttr('style').off('mouseout mouseover');
+             $(this).css({
+                 'border-color': '#222',
+                 'font-weight': 700,
+                 'background-color': '#c1c1c1'
+             });
+             if (submt) {
+                 submt = false;
+                 $('#submitbutton').css({
+                     'color': '#000'
+                 }).on('click', function () {
+                     $('.choice').off('click');
+                     $(this).off('click');
+                     processQuestion(picked);
+                 });
+             }
+         })
+     }
+
+
+     function endQuiz() {
+         $('#explanation').empty();
+         $('#question').empty();
+         $('#choice-block').empty();
+         $('#submitbutton').remove();
+         $('#question').text("Tu as " + score + " de " + quiz.length + " correcte.");
+         $(document.createElement('h2')).css({
+             'text-align': 'center',
+             'font-size': '4em'
+         }).text(Math.round(score / quiz.length * 100) + '%').insertAfter('#question');
+         $(document.getElementById("question-image")).css({
+             'display': 'none'
+         })
+     }
+
+
+     function init() {
+         //add title
+         if (typeof quiztitle !== "undefined" && $.type(quiztitle) === "string") {
+             $(document.createElement('h1')).text(quiztitle).appendTo('#frame');
+         } else {
+             $(document.createElement('h1')).text("Quiz").appendTo('#frame');
+         }
+
+         //add pager and questions
+         if (typeof quiz !== "undefined" && $.type(quiz) === "array") {
+             //add pager
+             $(document.createElement('p')).addClass('pager').attr('id', 'pager').text('Question 1 de ' + quiz.length).appendTo('#frame');
+             //add first question
+             $(document.createElement('h2')).addClass('question').attr('id', 'question').text(quiz[0]['question']).appendTo('#frame');
+             //add image if present
+             if (quiz[0].hasOwnProperty('image') && quiz[0]['image'] != "") {
+                 $(document.createElement('img')).addClass('question-image').attr('id', 'question-image').attr('src', quiz[0]['image']).attr('alt', htmlEncode(quiz[0]['question'])).appendTo('#frame');
+             }
+             $(document.createElement('p')).addClass('explanation').attr('id', 'explanation').html('&nbsp;').appendTo('#frame');
+
+             //questions holder
+             $(document.createElement('ul')).attr('id', 'choice-block').appendTo('#frame');
+
+             //add choices
+             addChoices(quiz[0]['choices']);
+
+             //add submit button
+             $(document.createElement('div')).addClass('choice-box').attr('id', 'submitbutton').text('Valider').css({
+                 'font-weight': 700,
+                 'color': '#fff',
+                 'padding': '30px 0'
+             }).appendTo('#frame');
+
+             setupButtons();
+         }
+     }
+
+     init();
+ });
